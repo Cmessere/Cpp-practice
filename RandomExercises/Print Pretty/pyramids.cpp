618526e7c924722427d53fa2e636ccf70a666881
Carlo Messere <c.messere@iconsulting.biz>
2020-04-07 01:33:36 +0200

    created pyramids printing

---
+++ RandomExercises/Print Pretty/pyramids.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    const short int base = 10;
+    const char mark = '*';
+
+    for(short int i = 0; i < 10; i++){
+        for(short int j=0; j < i; j++){
+            cout << mark;
+        }
+        cout << endl;
+    }
+}
