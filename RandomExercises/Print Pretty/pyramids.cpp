77b06e6641a9f260ba2047d7e5fecf526cda3bff
Carlo Messere <c.messere@iconsulting.biz>
2020-04-07 01:37:17 +0200

    extracted printPyramid function

--- RandomExercises/Print Pretty/pyramids.cpp
+++ RandomExercises/Print Pretty/pyramids.cpp
@@ -6,11 +6,18 @@ using namespace std;
 
 int main()
 {
+    printPyramid();
+}
+
+void printPyramid()
+{
     const short int base = 10;
     const char mark = '*';
 
-    for(short int i = 0; i < 10; i++){
-        for(short int j=0; j < i; j++){
+    for (short int i = 0; i < 10; i++)
+    {
+        for (short int j = 0; j < i; j++)
+        {
             cout << mark;
         }
         cout << endl;
