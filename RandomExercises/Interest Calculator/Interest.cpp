6998c47ca0e6ebb1f1dff41895939060d70e6bfa
Carlo Messere <c.messere@iconsulting.biz>
2020-04-14 20:47:20 +0200

    asking for variables values

--- RandomExercises/Interest Calculator/Interest.cpp
+++ RandomExercises/Interest Calculator/Interest.cpp
@@ -4,13 +4,27 @@ using namespace std;
 
 int main()
 {
+
+
     short months = 120;
     float interestRate = 0.01;
     double balance = 0, montlyDeposit = 50;
 
+    cout << "Insert starting balance: ";
+    cin >> balance;
+    
+    cout << "Insert number of months: ";
+    cin >> months;
+    
+    cout << "Insert interest rate: ";
+    cin >> interestRate;
+
+    cout << "Insert monthly deposit rate: ";
+    cin >> montlyDeposit;
+
     for(short i = 0; i < months; i++){
         balance = balance + balance*interestRate + montlyDeposit;
-        cout << "Month: " << i << "Balance: " << balance << endl;
+        cout << "Month: " << i << " Balance: " << balance << endl;
     }
 
 }
