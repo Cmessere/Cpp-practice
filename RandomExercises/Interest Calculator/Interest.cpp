b165c1f372279e97bbad9ca2ded7cbdebef7ffcb
Carlo Messere <c.messere@iconsulting.biz>
2020-04-14 20:59:48 +0200

    asking for type of interest

--- RandomExercises/Interest Calculator/Interest.cpp
+++ RandomExercises/Interest Calculator/Interest.cpp
@@ -1,13 +1,20 @@
 #include "interest.h"
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
-    short months = 120;
+    short months = 120, interestOption;
     float interestRate = 0.01;
     double balance = 0, monthlyDeposit = 50;
 
+
+    cout << "Select interest rate calculator: " << endl;
+    cout << "1) Deposit only." << endl;
+    cout << "2) Fixed Interest rate." << endl;
+    cout << "3) Variable interest rate" << endl;
+    cin >> interestOption;
     getValuesFromUser(balance, months, interestRate, monthlyDeposit);
 
     for(short i = 0; i < months; i++){
