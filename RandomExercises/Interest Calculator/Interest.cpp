31a88db5dd81edf526157c3136a82524be447dc9
Carlo Messere <c.messere@iconsulting.biz>
2020-04-15 01:06:10 +0200

    moved implementation in cpp file and fixed investedSum

--- RandomExercises/Interest Calculator/Interest.cpp
+++ RandomExercises/Interest Calculator/Interest.cpp
@@ -3,6 +3,49 @@
 
 using namespace std;
 
+void depositOnly(double &balance, short &months, double &monthlyDeposit, float &interestRate){
+    getValuesFromUser(balance, months, monthlyDeposit, interestRate=-1, 0);
+    
+    double investedSum = balance;
+
+    for(short i = 0; i < months; i++){
+        balance += monthlyDeposit;
+        cout << "Month: " << i << " Balance: " << balance << endl;
+        investedSum += monthlyDeposit;
+    }
+
+    cout << "Net gain: " << balance - investedSum;
+}
+void fixedRateInterest(double &balance, short &months, double &monthlyDeposit, float &interestRate){
+    getValuesFromUser(balance, months, monthlyDeposit, interestRate, 0);
+    
+    double investedSum = balance;
+
+    for(short i = 0; i < months; i++){
+        balance = balance + monthlyDeposit + balance*interestRate;
+        cout << "Month: " << i << " Balance: " << balance << endl;
+        investedSum += monthlyDeposit;
+    }
+
+    cout << "Net gain: " << balance - investedSum;
+}
+
+void variableRateInterest(double &balance, short &months, double &monthlyDeposit, float &interestRate, float &upperInterestRate){
+    getValuesFromUser(balance, months, monthlyDeposit, interestRate = -2, upperInterestRate);
+    
+    float randomInterestRate = (upperInterestRate*100 + interestRate*100)/((upperInterestRate*100 - interestRate*100));
+    double investedSum = balance;
+
+    for(short i = 0; i < months; i++){
+        balance = balance + monthlyDeposit + balance*(randomInterestRate/100);
+        cout << "Month: " << i << " Balance: " << balance << endl;
+        investedSum += monthlyDeposit;
+
+    }
+
+    cout << "Net gain: " << balance - investedSum;
+}
+
 int main()
 {
     short months, interestOption;
