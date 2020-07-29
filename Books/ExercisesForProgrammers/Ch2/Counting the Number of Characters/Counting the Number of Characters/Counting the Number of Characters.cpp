a181c05e0c5e8fa10798902781e83ef213e44e79
Carlo Messere <c.messere@iconsulting.biz>
2020-07-29 22:39:34 +0200

    format document

--- Books/ExercisesForProgrammers/Ch2/Counting the Number of Characters/Counting the Number of Characters/Counting the Number of Characters.cpp
+++ Books/ExercisesForProgrammers/Ch2/Counting the Number of Characters/Counting the Number of Characters/Counting the Number of Characters.cpp
@@ -9,14 +9,13 @@ int main()
     cout << "Give me a word, I'll give you its length!" << endl;
     getline(cin, prompt);
 
-    
 	while (prompt.empty()){
 
-            cout << "I need a word to give you a length, help me out here..." << endl;
+        cout << "I need a word to give you a length, help me out here..." << endl;
         getline(cin, prompt);
 
 	}
-
     cout << "The word " << prompt << " has " << prompt.length() << " letters in it.\nTry using it in a sentence!\n";
 
+    return 0;
 }
