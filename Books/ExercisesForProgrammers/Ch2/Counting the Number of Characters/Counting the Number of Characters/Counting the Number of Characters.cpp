b081f8e4ad0f866ae8a25822cb383d7b30514a33
Carlo Messere <c.messere@iconsulting.biz>
2020-07-29 22:30:24 +0200

    reprompt if input is empty
    No GUI version sadly :(

--- Books/ExercisesForProgrammers/Ch2/Counting the Number of Characters/Counting the Number of Characters/Counting the Number of Characters.cpp
+++ Books/ExercisesForProgrammers/Ch2/Counting the Number of Characters/Counting the Number of Characters/Counting the Number of Characters.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main()
 {
     string prompt;
-
     cout << "Give me a word, I'll give you its length!" << endl;
-    cin >> prompt;
+    getline(cin, prompt);
+
+    
+	while (prompt.empty()){
+
+            cout << "I need a word to give you a length, help me out here..." << endl;
+        getline(cin, prompt);
+
+	}
+
     cout << "The word " << prompt << " has " << prompt.length() << " letters in it.\nTry using it in a sentence!\n";
 
 }
