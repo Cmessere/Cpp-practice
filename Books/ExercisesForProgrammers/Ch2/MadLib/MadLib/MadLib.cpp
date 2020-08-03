9d5dea0fbba3d101134926168a6d89aaf4611650
Carlo Messere <messerecarlo27@gmail.com>
2020-08-04 01:19:51 +0200

    basic MadLib implementation

--- Books/ExercisesForProgrammers/Ch2/MadLib/MadLib/MadLib.cpp
+++ Books/ExercisesForProgrammers/Ch2/MadLib/MadLib/MadLib.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string noun, verb, adjective, adverb;
+
+    cout << "Enter a noun :" << endl;
+    cin >> noun;
+    cout << "Enter a verb :" << endl;
+    cin >> verb;
+    cout << "Enter an adjective :" << endl;
+    cin >> adjective;
+    cout << "Enter an adverb :" << endl;
+    cin >> adverb;
+
+    cout << "Do you " << verb << " your " << adjective << " " << noun << " "  << adverb << "? That's hilarious!" << endl;
 }
