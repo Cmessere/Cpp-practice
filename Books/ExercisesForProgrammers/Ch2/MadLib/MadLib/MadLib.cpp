42aa87ae198937376aa4770442b24540e304acf0
Carlo Messere <messerecarlo27@gmail.com>
2020-08-04 01:37:46 +0200

    conditional MadLib

--- Books/ExercisesForProgrammers/Ch2/MadLib/MadLib/MadLib.cpp
+++ Books/ExercisesForProgrammers/Ch2/MadLib/MadLib/MadLib.cpp
@@ -1,19 +1,43 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+void initializeQuestions(vector<string>& q) {
+    q.push_back("Enter a verb :");
+    q.push_back("Enter a noun :");
+    q.push_back("Enter a adjective :");
+    q.push_back("Enter another adjective :");
+    q.push_back("Enter an adverb :");
+    q.push_back("Enter another adverb :");
+    q.push_back("Enter a number :");
+}
+
+void askForWords(vector<string>& questions, vector<string>& words) {
+    string temp;
+
+    for (auto c : questions) {
+        cout << c << endl;
+        cin >> temp;
+        words.push_back(temp);
+     }
+}
+
+void generatePhraseOne( const vector<string>& words) {
+    cout << "Do you " << words[0] << " your " << words[1] << " " << words[2] << " " << words[3] << " and " << words[4] << "? That's " << words[5] << " hilarious!" << endl;
+
+}
+
+void generatePhraseTwo(const vector<string>& words) {
+    cout << "Hey were you " << words[0] << " before? was it because of that " << words[3] << " " << words[2] << " " << words[1] << "? It's " << words[4] <<  " hard to " << words[5] << " know." << endl;
+}
 
 int main()
 {
-    string noun, verb, adjective, adverb;
+    vector<string> words, questions;
+    string input;
+    
+    initializeQuestions(questions);
+    askForWords(questions, words);
+    words[6].size() % 2 == 0? generatePhraseOne(words) : generatePhraseTwo(words);
 
-    cout << "Enter a noun :" << endl;
-    cin >> noun;
-    cout << "Enter a verb :" << endl;
-    cin >> verb;
-    cout << "Enter an adjective :" << endl;
-    cin >> adjective;
-    cout << "Enter an adverb :" << endl;
-    cin >> adverb;
-
-    cout << "Do you " << verb << " your " << adjective << " " << noun << " "  << adverb << "? That's hilarious!" << endl;
 }
