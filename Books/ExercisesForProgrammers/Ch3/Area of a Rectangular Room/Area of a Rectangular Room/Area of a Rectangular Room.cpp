bed6962d58dd8772bfb3a25b66eff93d561abbc1
Carlo Messere <messerecarlo27@gmail.com>
2020-08-06 00:26:37 +0200

    Keep asking for numbers

--- Books/ExercisesForProgrammers/Ch3/Area of a Rectangular Room/Area of a Rectangular Room/Area of a Rectangular Room.cpp
+++ Books/ExercisesForProgrammers/Ch3/Area of a Rectangular Room/Area of a Rectangular Room/Area of a Rectangular Room.cpp
@@ -2,15 +2,29 @@
 
 using namespace std;
 
+double getInput(const string& dimension) {
+    double size;
+
+    cout << "What is the "<< dimension <<" of the room in feet?" << endl;
+    cin >> size;
+
+    while (cin.fail())
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Only numbers allowed. Please try again" << endl;
+        cin >> size;
+    }
+    return size;
+}
+
 int main()
 {
     double length, width;
     const double SQUARED_FEET_TO_METERS = 0.09290304; 
 
-    cout << "What is the length of the room in feet?" << endl;
-    cin >> length;
-    cout << "What is the width of the room in feet?" << endl;
-    cin >> width;
+    length = getInput("length");
+    width = getInput("width");
 
     cout << "You entered dimensions of " << length << " feet by " << width << " feet." << endl;
 
