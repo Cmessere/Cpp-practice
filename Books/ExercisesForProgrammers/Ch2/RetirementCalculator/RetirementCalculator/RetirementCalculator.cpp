48fe00516b2fd1758f7344d69ed441887e9788ce
Carlo Messere <messerecarlo27@gmail.com>
2020-08-05 23:52:17 +0200

    Letting the user know if he can already retire

--- Books/ExercisesForProgrammers/Ch2/RetirementCalculator/RetirementCalculator/RetirementCalculator.cpp
+++ Books/ExercisesForProgrammers/Ch2/RetirementCalculator/RetirementCalculator/RetirementCalculator.cpp
@@ -5,24 +5,39 @@
 using std::chrono::system_clock;
 using namespace std;
 
+void printRetirement(const int& remaining, const int& current) {
+
+    if (remaining < 0) {
+        cout << "Oh! you can already retire!" << endl;
+    }
+    else {
+        cout << "You have " << remaining << " Years before you can retire." << endl;
+        cout << "It's " << current << ". so you can retire in " << current + remaining << " before retiring" << endl;
+    }
+}
+
+int getCurrentYear() {
+    system_clock::time_point now = system_clock::now();
+    time_t tt = system_clock::to_time_t(now);
+
+    struct tm timeinfo;
+    localtime_s(&timeinfo, &tt);
+
+    return timeinfo.tm_year + 1900;
+}
+
 int main()
 {
     string currentAge, retirementAge;
 
-    system_clock::time_point now = system_clock::now();
-    time_t tt = system_clock::to_time_t(now);
-    struct tm timeinfo;
-    localtime_s(&timeinfo, &tt);
+    int currentYear = getCurrentYear();
 
-    int currentYear = timeinfo.tm_year + 1900;
-
-    cout << "What is your age?" <<endl;
+    cout << "What is your age?" << endl;
     cin >> currentAge;
     cout << "At what age would you like to retire?" << endl;
     cin >> retirementAge;
 
-    int remainingYears = stoi(retirementAge) - stoi(currentAge)
+    int remainingYears = stoi(retirementAge) - stoi(currentAge);
 
-    cout << "You have " << remainingYears << " Years before you can retire." << endl;
-    cout << "It's " << currentYear << ". so you can retire in " << currentYear + remainingYears << " before retiring" << endl;
+    printRetirement(remainingYears, currentYear);
 }
