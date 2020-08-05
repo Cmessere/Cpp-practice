726a08f62bee85b8e232e03afdf855377b83e425
Carlo Messere <messerecarlo27@gmail.com>
2020-08-05 23:39:23 +0200

    basic Retirement Calculator implementation

--- Books/ExercisesForProgrammers/Ch2/RetirementCalculator/RetirementCalculator/RetirementCalculator.cpp
+++ Books/ExercisesForProgrammers/Ch2/RetirementCalculator/RetirementCalculator/RetirementCalculator.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
-
+#include <string>
+#include <ctime>
+#include <chrono>
+using std::chrono::system_clock;
 using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string currentAge, retirementAge;
+
+    system_clock::time_point now = system_clock::now();
+    time_t tt = system_clock::to_time_t(now);
+    struct tm timeinfo;
+    localtime_s(&timeinfo, &tt);
+
+    int currentYear = timeinfo.tm_year + 1900;
+
+    cout << "What is your age?" <<endl;
+    cin >> currentAge;
+    cout << "At what age would you like to retire?" << endl;
+    cin >> retirementAge;
+
+    int remainingYears = stoi(retirementAge) - stoi(currentAge)
+
+    cout << "You have " << remainingYears << " Years before you can retire." << endl;
+    cout << "It's " << currentYear << ". so you can retire in " << currentYear + remainingYears << " before retiring" << endl;
 }
