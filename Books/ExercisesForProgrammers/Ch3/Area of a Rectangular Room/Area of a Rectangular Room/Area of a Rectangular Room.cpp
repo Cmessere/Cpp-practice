ea44173b6b0a085f224f2101fcde6d091fe2733a
Carlo Messere <messerecarlo27@gmail.com>
2020-08-06 00:10:21 +0200

    Area of a Rectangular Room basic implementation

--- Books/ExercisesForProgrammers/Ch3/Area of a Rectangular Room/Area of a Rectangular Room/Area of a Rectangular Room.cpp
+++ Books/ExercisesForProgrammers/Ch3/Area of a Rectangular Room/Area of a Rectangular Room/Area of a Rectangular Room.cpp
@@ -4,5 +4,17 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double length, width;
+    const double SQUARED_FEET_TO_METERS = 0.09290304; 
+
+    cout << "What is the length of the room in feet?" << endl;
+    cin >> length;
+    cout << "What is the width of the room in feet?" << endl;
+    cin >> width;
+
+    cout << "You entered dimensions of " << length << " feet by " << width << " feet." << endl;
+
+    cout << "The Area is:" << endl;
+    cout << length * width << " square feet" << endl;
+    cout << (length * width)*SQUARED_FEET_TO_METERS << " square feet" << endl;
 }
