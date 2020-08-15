58744a5a62a297bb8f9d326b533e5921bdc783d0
Carlo Messere <messerecarlo27@gmail.com>
2020-08-15 19:06:27 +0200

    basic Paint Calculator implementation

--- Books/ExercisesForProgrammers/Ch3/Paint Calculator/Paint Calculator/Paint Calculator.cpp
+++ Books/ExercisesForProgrammers/Ch3/Paint Calculator/Paint Calculator/Paint Calculator.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
+constexpr auto FEET_SQUARED_PER_GALLON = 350;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	double length, width;
+	unsigned short paintCans = 1;
+
+	cout << "What is the lenght of the room in feet?" << endl;
+	cin >> length;
+	cout << "What is the width of the room in feet?" << endl;
+	cin >> width;
+
+	paintCans = ceil(length * width / FEET_SQUARED_PER_GALLON);
+
+	cout << "You're going to need " << paintCans << " to completely paint the room." << endl;
 }
