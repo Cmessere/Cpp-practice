195e74839905f425350d842d29d995bea2e306ed
Carlo Messere <messerecarlo27@gmail.com>
2020-08-15 19:11:55 +0200

    only numeric values allowed

--- Books/ExercisesForProgrammers/Ch3/Paint Calculator/Paint Calculator/Paint Calculator.cpp
+++ Books/ExercisesForProgrammers/Ch3/Paint Calculator/Paint Calculator/Paint Calculator.cpp
@@ -4,16 +4,29 @@
 using namespace std;
 constexpr auto FEET_SQUARED_PER_GALLON = 350;
 
+double getInput(const string& dimension) {
+    double size;
+
+    cout << "What is the " << dimension << " of the room?" << endl;
+    cin >> size;
+
+    while (cin.fail())
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Only numbers allowed. Please try again" << endl;
+        cin >> size;
+    }
+    return size;
+}
 
 int main()
 {
 	double length, width;
 	unsigned short paintCans = 1;
 
-	cout << "What is the lenght of the room in feet?" << endl;
-	cin >> length;
-	cout << "What is the width of the room in feet?" << endl;
-	cin >> width;
+    length = getInput("length");
+    width = getInput("width");
 
 	paintCans = ceil(length * width / FEET_SQUARED_PER_GALLON);
 
