4ee90e881809f0d5ce2f0527dd56e1cee735ed3b
Carlo Messere <messerecarlo27@gmail.com>
2020-08-17 01:49:43 +0200

    Allowing upper, lower or mixed state code

--- Books/ExercisesForProgrammers/Ch4/TaxCalculator/TaxCalculator/TaxCalculator.cpp
+++ Books/ExercisesForProgrammers/Ch4/TaxCalculator/TaxCalculator/TaxCalculator.cpp
@@ -1,17 +1,46 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+
 constexpr auto STATE_TAX = 5.5;
 
 using namespace std;
+
+double getDouble() {
+	double input;
+
+	cout << "What is the order amount? ";
+	cin >> input;
+
+	while (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Only numbers allowed. Please try again" << endl;
+		cin >> input;
+	}
+	return input;
+}
+
+string getString() {
+	string input;
+
+	cout << "What is the state? ";
+	cin >> input;
+
+	return input;
+}
 
 int main()
 {
 	double amount;
 	string state;
 
-	cout << "What is the order amount? ";
-	cin >> amount;
-	cout << "What is the state? ";
-	cin >> state;
+	amount = getDouble();
+	state = getString();
+
+	std::for_each(state.begin(), state.end(), [](char& c) {c = ::toupper(c); });
+
 	if (state == "WI") {
 		cout << "The subtotal is $" << amount << endl;
 		cout << "The tax is $" << amount/100*STATE_TAX << endl;
