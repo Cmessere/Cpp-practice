6b974b3efbbe0b1a0402007c5203f3f7c9d4b73d
Carlo Messere <messerecarlo27@gmail.com>
2020-08-16 17:20:53 +0200

    fetching exchange rates with http request

--- Books/ExercisesForProgrammers/Ch3/CurrencyConverter/CurrencyConverter/CurrencyConverter.cpp
+++ Books/ExercisesForProgrammers/Ch3/CurrencyConverter/CurrencyConverter/CurrencyConverter.cpp
@@ -3,51 +3,86 @@
 #include <string>
 #include <algorithm>
 #include "CurrencyConverter.h"
+#include <curl/curl.h>
+#include <json/json.h> 
 
 using namespace std;
 
-void initializeRates(map<string, double>& rates) {
-    rates["GBP"] = 1.308527;
-    rates["USD"] = 1;
-    rates["EUR"] = 1.184204;
-    rates["AUD"] = 0.717205;
-    rates["CAN"] = 0.753668;
-    rates["JPY"] = 0.009384;
-    rates["CNY"] = 0.143871;
-}
-
-void initializeCountries( map<string, string> &currencies) {
-    currencies["UK"] = "GBP";
-    currencies["US"] = "USD";
-    currencies["CA"] = "CAN";
-    currencies["AU"] = "AUD";
-    currencies["IT"] = "EUR";
-    currencies["FR"] = "EUR";
-    currencies["DE"] = "EUR";
-    currencies["JP"] = "JPY";
-    currencies["CH"] = "CNY";
-}
-
 int main()
 {
-    map<string, string> currencies;
-    map<string, double> rates;
+    map<string, double> ratesApi;
+
+    bool retflag;
+    int retval = fetchExchangeRates(ratesApi, retflag);
+    if (retflag) return retval;
+
+    map<string, string> countryToCurrency;
     string inputCountry;
     double quantity;
 
-    initializeCountries(currencies);
-    initializeRates(rates);
+    initializeCountries(countryToCurrency);
 
-    getInputCountry(inputCountry, currencies);
+    getInputCountry(inputCountry, countryToCurrency, countryToCurrency);
 
-    cout << inputCountry << " currency is the " << currencies[inputCountry] << endl;
+    cout << inputCountry << " currency is the " << countryToCurrency[inputCountry] << endl;
 
     getExchangeQuantity(quantity);
 
-    string selectedCurrency = currencies[inputCountry];
-    double exchangeRate = rates[selectedCurrency];
+    string selectedCurrency = countryToCurrency[inputCountry];
+    double exchangeRate = ratesApi[selectedCurrency];
 
     outputEchangeValue(quantity, selectedCurrency, exchangeRate);
+}
+
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+
+int fetchExchangeRates(std::map<std::string, double>& ratesApi, bool& retflag)
+{
+    retflag = true;
+    CURL* curl;
+    CURLcode res;
+    string readBuffer;
+    curl = curl_easy_init();
+
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.exchangeratesapi.io/latest?base=USD");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+    }
+
+    if (res == CURLE_OK) {
+        Json::Value root;
+        Json::CharReaderBuilder builder;
+        const auto rawJsonLength = static_cast<int>(readBuffer.length());
+        JSONCPP_STRING err;
+
+        const std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+        if (!reader->parse(readBuffer.c_str(), readBuffer.c_str() + rawJsonLength, &root,
+            &err)) {
+            std::cout << "error" << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        const Json::Value& ratesJson = root["rates"];
+
+        for (auto const& id : ratesJson.getMemberNames()) {
+            ratesApi[id] = ratesJson[id].asDouble();
+        }
+    }
+    else {
+        cerr << "Could not fetch exchange rates, check internet connection and try again\n";
+        return 1;
+    }
+
+    retflag = false;
+    return {};
 }
 
 void getExchangeQuantity(double& quantity)
@@ -58,21 +93,40 @@ void getExchangeQuantity(double& quantity)
 
 void outputEchangeValue(double quantity, std::string& selectedCurrency, double exchangeRate)
 {
-    cout.precision(5);
+    cout.precision(2);
+    cout << std::fixed;
+
     cout << quantity << " " << selectedCurrency << " at an exchange rate of " << exchangeRate << " is " <<
-        quantity * exchangeRate << " dollars.\n";
+        quantity / exchangeRate << " dollars.\n";
 }
 
-void getInputCountry(std::string& inputCountry, std::map<std::string, std::string>& currencies)
+void getInputCountry(std::string& inputCountry, std::map<std::string, std::string>& currencies, map<string, string> & countryToCurrency)
 {
-    cout << "Insert the country for the exchange rate( ex UK, US, or IT): ";
+    cout << "Insert the country code for the exchange rate (write list for help): ";
     cin >> inputCountry;
 
-    std::for_each(inputCountry.begin(), inputCountry.end(), [](char& c) {c = ::toupper(c); });
+    inputToUpperCase(inputCountry);
 
+    
     while (!currencies.count(inputCountry))
     {
-        cout << "Sorry the inserted code is invalid or not implemented, please try again.\n";
-        cin >> inputCountry;
+        if (inputCountry == "LIST") {
+            for (map<string, string>::iterator it = countryToCurrency.begin(); it != countryToCurrency.end(); ++it) {
+                cout << it->first << "\n";
+            }
+            cin >> inputCountry;
+            inputToUpperCase(inputCountry);
+        }
+        else {
+            cout << "Sorry the inserted code is invalid or not implemented, please try again.\n";
+            cin >> inputCountry;
+            inputToUpperCase(inputCountry);
+        }
+        
     }
+}
+
+void inputToUpperCase(std::string& inputCountry)
+{
+    std::for_each(inputCountry.begin(), inputCountry.end(), [](char& c) {c = ::toupper(c); });
 }
--- Books/ExercisesForProgrammers/Ch3/CurrencyConverter/CurrencyConverter/CurrencyConverter.h
+++ Books/ExercisesForProgrammers/Ch3/CurrencyConverter/CurrencyConverter/CurrencyConverter.h
@@ -1,5 +1,70 @@
-void getInputCountry(std::string& inputCountry, std::map<std::string, std::string>& currencies);
+#include <vector>
+
+void getInputCountry(std::string& inputCountry, std::map<std::string, std::string>& currencies, std::map<std::string, std::string>& countryList);
+
+void inputToUpperCase(std::string& inputCountry);
 
 void outputEchangeValue(double quantity, std::string& selectedCurrency, double exchangeRate);
 
 void getExchangeQuantity(double& quantity);
+
+int fetchExchangeRates(std::map<std::string, double>& ratesApi, bool& retflag);
+
+void initializeCountries(std::map<std::string, std::string>& currencies);
+
+void initializeCountries(std::map<std::string, std::string>& currencies)
+{
+    currencies["US"] = "USD";
+    currencies["UK"] = "GBP";
+    currencies["HK"] = "HDK";
+    currencies["IS"] = "ISK";
+    currencies["PH"] = "PHP";
+    currencies["DK"] = "DKK";
+    currencies["HU"] = "HUF";
+    currencies["CZ"] = "CZK";
+    currencies["RO"] = "RON";
+    currencies["SE"] = "SEK";
+    currencies["RI"] = "IDR";
+    currencies["IN"] = "INR";
+    currencies["BR"] = "BRL";
+    currencies["RU"] = "RUB";
+    currencies["HR"] = "HRK";
+    currencies["MAL"] = "MYR";
+    currencies["T"] = "THB";
+    currencies["CH"] = "CHF";
+    currencies["BG"] = "BGN";
+    currencies["TR"] = "TRY";
+    currencies["N"] = "NOK";
+    currencies["NZ"] = "NZD";
+    currencies["ZA"] = "ZAR";
+    currencies["MX"] = "MXN";
+    currencies["SG"] = "SGD";
+    currencies["IL"] = "ILS";
+    currencies["ROK"] = "KRW";
+    currencies["PL"] = "PLN";
+    currencies["CA"] = "CAN";
+    currencies["AU"] = "AUD";
+
+    currencies["IT"] = "EUR";
+    currencies["FR"] = "EUR";
+    currencies["DE"] = "EUR";
+    currencies["E"] = "EUR";
+    currencies["P"] = "EUR";
+    currencies["A"] = "EUR";
+    currencies["B"] = "EUR";
+    currencies["CY"] = "EUR";
+    currencies["EST"] = "EUR";
+    currencies["FIN"] = "EUR";
+    currencies["GR"] = "EUR";
+    currencies["IRL"] = "EUR";
+    currencies["LT"] = "EUR";
+    currencies["LV"] = "EUR";
+    currencies["L"] = "EUR";
+    currencies["M"] = "EUR";
+    currencies["NL"] = "EUR";
+    currencies["SLO"] = "EUR";
+    currencies["SK"] = "EUR";
+
+    currencies["JP"] = "JPY";
+    currencies["CH"] = "CNY";
+}
