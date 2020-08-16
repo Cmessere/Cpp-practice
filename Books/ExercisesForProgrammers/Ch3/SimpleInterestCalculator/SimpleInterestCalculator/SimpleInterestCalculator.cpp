295c3bd75663c9c3668958c113e11055156428ab
Carlo Messere <messerecarlo27@gmail.com>
2020-08-16 17:33:29 +0200

    basic implementation

--- Books/ExercisesForProgrammers/Ch3/SimpleInterestCalculator/SimpleInterestCalculator/SimpleInterestCalculator.cpp
+++ Books/ExercisesForProgrammers/Ch3/SimpleInterestCalculator/SimpleInterestCalculator/SimpleInterestCalculator.cpp
@@ -1,20 +1,20 @@
-// SimpleInterestCalculator.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	double rate, startingCapital, calculatedAmount;
+	unsigned short investmentYears;
+
+	cout << "Enter the principal: ";
+	cin >> startingCapital;
+	cout << "Enter the rate of interest: ";
+	cin >> rate;
+	cout << "Enter the number of years: ";
+	cin >> investmentYears;
+
+	calculatedAmount = startingCapital*(1 + rate / 100 * investmentYears);
+
+	cout << "After " << investmentYears << " years at " << rate << "%, the investment will be worth $" << calculatedAmount;
 }
-
-// Per eseguire il programma: CTRL+F5 oppure Debug > Avvia senza eseguire debug
-// Per eseguire il debug del programma: F5 oppure Debug > Avvia debug
-
-// Suggerimenti per iniziare: 
-//   1. Usare la finestra Esplora soluzioni per aggiungere/gestire i file
-//   2. Usare la finestra Team Explorer per connettersi al controllo del codice sorgente
-//   3. Usare la finestra di output per visualizzare l'output di compilazione e altri messaggi
-//   4. Usare la finestra Elenco errori per visualizzare gli errori
-//   5. Passare a Progetto > Aggiungi nuovo elemento per creare nuovi file di codice oppure a Progetto > Aggiungi elemento esistente per aggiungere file di codice esistenti al progetto
-//   6. Per aprire di nuovo questo progetto in futuro, passare a File > Apri > Progetto e selezionare il file con estensione sln
