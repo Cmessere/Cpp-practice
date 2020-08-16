18e4f300eabf6438f20f1b5bbc22603f749714f9
Carlo Messere <messerecarlo27@gmail.com>
2020-08-16 03:51:58 +0200

    getting exchange rate by country code

--- Books/ExercisesForProgrammers/Ch3/CurrencyConverter/CurrencyConverter/CurrencyConverter.cpp
+++ Books/ExercisesForProgrammers/Ch3/CurrencyConverter/CurrencyConverter/CurrencyConverter.cpp
@@ -1,15 +1,78 @@
 #include <iostream>
+#include <map>
+#include <string>
+#include <algorithm>
+#include "CurrencyConverter.h"
 
 using namespace std;
 
+void initializeRates(map<string, double>& rates) {
+    rates["GBP"] = 1.308527;
+    rates["USD"] = 1;
+    rates["EUR"] = 1.184204;
+    rates["AUD"] = 0.717205;
+    rates["CAN"] = 0.753668;
+    rates["JPY"] = 0.009384;
+    rates["CNY"] = 0.143871;
+}
+
+void initializeCountries( map<string, string> &currencies) {
+    currencies["UK"] = "GBP";
+    currencies["US"] = "USD";
+    currencies["CA"] = "CAN";
+    currencies["AU"] = "AUD";
+    currencies["IT"] = "EUR";
+    currencies["FR"] = "EUR";
+    currencies["DE"] = "EUR";
+    currencies["JP"] = "JPY";
+    currencies["CH"] = "CNY";
+}
+
 int main()
 {
-    double euro, rate;
+    map<string, string> currencies;
+    map<string, double> rates;
+    string inputCountry;
+    double quantity;
 
-    cout << "How many euros are you exchanging? ";
-    cin >> euro;
-    cout << "What is the exchange rate? ";
-    cin >> rate;
+    initializeCountries(currencies);
+    initializeRates(rates);
 
-    cout << euro << " euros at an exchange rate of " << rate << " is " << euro * rate / 100 << " dollars.\n";
+    getInputCountry(inputCountry, currencies);
+
+    cout << inputCountry << " currency is the " << currencies[inputCountry] << endl;
+
+    getExchangeQuantity(quantity);
+
+    string selectedCurrency = currencies[inputCountry];
+    double exchangeRate = rates[selectedCurrency];
+
+    outputEchangeValue(quantity, selectedCurrency, exchangeRate);
+}
+
+void getExchangeQuantity(double& quantity)
+{
+    cout << "How much are you exchanging? ";
+    cin >> quantity;
+}
+
+void outputEchangeValue(double quantity, std::string& selectedCurrency, double exchangeRate)
+{
+    cout.precision(5);
+    cout << quantity << " " << selectedCurrency << " at an exchange rate of " << exchangeRate << " is " <<
+        quantity * exchangeRate << " dollars.\n";
+}
+
+void getInputCountry(std::string& inputCountry, std::map<std::string, std::string>& currencies)
+{
+    cout << "Insert the country for the exchange rate( ex UK, US, or IT): ";
+    cin >> inputCountry;
+
+    std::for_each(inputCountry.begin(), inputCountry.end(), [](char& c) {c = ::toupper(c); });
+
+    while (!currencies.count(inputCountry))
+    {
+        cout << "Sorry the inserted code is invalid or not implemented, please try again.\n";
+        cin >> inputCountry;
+    }
 }
---
+++ Books/ExercisesForProgrammers/Ch3/CurrencyConverter/CurrencyConverter/CurrencyConverter.h
@@ -0,0 +1,5 @@
+void getInputCountry(std::string& inputCountry, std::map<std::string, std::string>& currencies);
+
+void outputEchangeValue(double quantity, std::string& selectedCurrency, double exchangeRate);
+
+void getExchangeQuantity(double& quantity);
