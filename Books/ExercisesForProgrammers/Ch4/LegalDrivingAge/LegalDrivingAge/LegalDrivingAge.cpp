773f77573acae048c7f41053e9165646932c8c87
Carlo Messere <messerecarlo27@gmail.com>
2020-08-17 04:49:45 +0200

    only positive numbers

--- Books/ExercisesForProgrammers/Ch4/LegalDrivingAge/LegalDrivingAge/LegalDrivingAge.cpp
+++ Books/ExercisesForProgrammers/Ch4/LegalDrivingAge/LegalDrivingAge/LegalDrivingAge.cpp
@@ -4,10 +4,18 @@ using namespace std;
 
 int main()
 {
-    unsigned short age;
+    short age;
 
     cout << "What is your age? ";
     cin >> age;
 
+    while (cin.fail() || age < 0)
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Only positive numbers allowed. Please try again" << endl;
+        cin >> age;
+    }
+    
     age < 15 ? cout << "You are not old enough to legally drive." : cout << "You are old enough to legally drive.";
 }
