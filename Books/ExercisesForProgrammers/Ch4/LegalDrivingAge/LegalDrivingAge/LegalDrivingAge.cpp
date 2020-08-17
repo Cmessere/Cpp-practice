4b26113f38a550cc9ca486b81e52c84aca5666ef
Carlo Messere <messerecarlo27@gmail.com>
2020-08-17 04:46:32 +0200

    basic implementation

--- Books/ExercisesForProgrammers/Ch4/LegalDrivingAge/LegalDrivingAge/LegalDrivingAge.cpp
+++ Books/ExercisesForProgrammers/Ch4/LegalDrivingAge/LegalDrivingAge/LegalDrivingAge.cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    unsigned short age;
+
+    cout << "What is your age? ";
+    cin >> age;
+
+    age < 15 ? cout << "You are not old enough to legally drive." : cout << "You are old enough to legally drive.";
 }
