058b874e40621c868521ef2ca21f8b181e018117
Carlo Messere <messerecarlo27@gmail.com>
2020-08-17 03:40:38 +0200

    masking input with asterisks (Only windows)

--- Books/ExercisesForProgrammers/Ch4/PasswordValidation/PasswordValidation/PasswordValidation.cpp
+++ Books/ExercisesForProgrammers/Ch4/PasswordValidation/PasswordValidation/PasswordValidation.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <string>
+#include <windows.h>
 
 using namespace std;
+string getpass(const char* prompt, bool show_asterisk);
 
 int main()
 {
-	string password;
 
-	cout << "What is the password? ";
-	cin >> password;
+	string password = getpass("Please enter the password: ", true); // Show asterisks
 
 	if (password == "abc$123") {
 		cout << "Welcome!" << endl;
@@ -15,4 +16,44 @@ int main()
 	else {
 		cout << "I don't know you" << endl;
 	}
+}
+
+string getpass(const char* prompt, bool show_asterisk = true)
+{
+    const char BACKSPACE = 8;
+    const char RETURN = 13;
+
+    string password;
+    unsigned char ch = 0;
+
+    cout << prompt << endl;
+
+    DWORD con_mode;
+    DWORD dwRead;
+
+    HANDLE hIn = GetStdHandle(STD_INPUT_HANDLE);
+
+    GetConsoleMode(hIn, &con_mode);
+    SetConsoleMode(hIn, con_mode & ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT));
+
+    while (ReadConsoleA(hIn, &ch, 1, &dwRead, NULL) && ch != RETURN)
+    {
+        if (ch == BACKSPACE)
+        {
+            if (password.length() != 0)
+            {
+                if (show_asterisk)
+                    cout << "\b \b";
+                password.resize(password.length() - 1);
+            }
+        }
+        else
+        {
+            password += ch;
+            if (show_asterisk)
+                cout << '*';
+        }
+    }
+    cout << endl;
+    return password;
 }
