74cb2c3aed89f21e4fa513cbe0e838faf4854f21
Carlo Messere <messerecarlo27@gmail.com>
2020-08-18 01:27:51 +0200

    TemperatureConverter implementation

---
+++ Books/ExercisesForProgrammers/Ch4/TemperatureConverter/TemperatureConverter/TemperatureConverter.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void fahrenheitToCelsius();
+void promptForConversion(std::string& choice);
+void celsiusToFahrenheit();
+
+int main()
+{
+	string choice;
+
+	promptForConversion(choice);
+
+	choice == "C" ? fahrenheitToCelsius() : celsiusToFahrenheit();
+}
+
+void promptForConversion(std::string& choice)
+{
+	cout << "Press C to convert from Fahrenheit to Celsius." << endl;
+	cout << "Press F to convert from Celsius to Fahrenheit." << endl;
+	cout << "Your choice: ";
+	cin >> choice;
+
+	std::for_each(choice.begin(), choice.end(), [](char& c) { c = ::toupper(c); });
+
+	while (choice != "C" && choice != "F") {
+		cout << "This is not a valid option, please try again." << endl;
+		cin >> choice;
+		std::for_each(choice.begin(), choice.end(), [](char& c) { c = ::toupper(c); });
+	}
+}
+
+void fahrenheitToCelsius() {
+	double temperature;
+
+	cout << "Please enter the temperature in Fahrenheit: ";
+	cin >> temperature;
+
+	temperature = (temperature - 32) * 5 / 9;
+
+	cout << "The temperature in celsius is " << temperature << endl;
+}
+
+void celsiusToFahrenheit() {
+	double temperature;
+
+	cout << "Please enter the temperature in Celsius: ";
+	cin >> temperature;
+
+	temperature = (temperature * 9/5) + 32;
+
+	cout << "The temperature in celsius is " << temperature << endl;
+}
