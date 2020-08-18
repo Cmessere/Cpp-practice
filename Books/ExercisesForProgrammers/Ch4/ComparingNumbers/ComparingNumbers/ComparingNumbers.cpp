e61bf40ce82461a0664fd18ebd5c8143a8223e70
Carlo Messere <messerecarlo27@gmail.com>
2020-08-18 04:04:41 +0200

    Asking for unlimited numbers

--- Books/ExercisesForProgrammers/Ch4/ComparingNumbers/ComparingNumbers/ComparingNumbers.cpp
+++ Books/ExercisesForProgrammers/Ch4/ComparingNumbers/ComparingNumbers/ComparingNumbers.cpp
@@ -3,11 +3,14 @@
 
 using namespace std;
 
-double getDouble(const short question) {
+double getDouble(const short question, bool &keepAsking) {
 	double input;
 
 	cout << "Enter the "<< question << " number: ";
 	cin >> input;
+
+	if (input == -1)
+		keepAsking = false;
 
 	while (cin.fail())
 	{
@@ -23,14 +26,18 @@ int main()
 {
 	double input, max = std::numeric_limits<double>::min();
 	set<int> inputSet;
+	short numberCount = 0;
+	bool keepAsking = true;
 
-	for (short i = 0; i < 10; i++) {
-		input = getDouble(i + 1);
+	cout << "Keep adding numbers, insert -1 to stop." << endl;
+
+	while(keepAsking){
+		input = getDouble(numberCount + 1, keepAsking);
 		bool alreadyInserted = inputSet.find(input) != inputSet.end();
-
+		
 		while (alreadyInserted) {
 			cout << "You already entered "<< input << endl;
-			input = getDouble(i + 1);
+			input = getDouble(numberCount + 1, keepAsking);
 			alreadyInserted = inputSet.find(input) != inputSet.end();
 		}
 
@@ -39,6 +46,8 @@ int main()
 		if (input > max) {
 			max = input;
 		}
+
+		numberCount++;
 	}
 	
 	cout << "The largest number is: " << max << endl;
