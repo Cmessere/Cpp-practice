5fe1387f144ddddbb5d3be76e35928419cb8e2e3
Carlo Messere <messerecarlo27@gmail.com>
2020-08-19 01:15:42 +0200

    asking for output form and max size

--- Books/ExercisesForProgrammers/Ch6/MultiplicationTable/MultiplicationTable/MultiplicationTable.cpp
+++ Books/ExercisesForProgrammers/Ch6/MultiplicationTable/MultiplicationTable/MultiplicationTable.cpp
@@ -1,17 +1,66 @@
 #include <iostream>
 #include <vector>
-
-constexpr auto SIZE = 12;
+#include <algorithm>
+#include <iomanip>
 
 using namespace std;
 
-int main()
-{
+void printList(const int& size);
+void printTable(const int& size);
 
-    for (int i = 0; i <= SIZE; i++) {
-        for (int j = 0; j <= SIZE; j++) {
+void printList(const int& size) {
+    for (int i = 0; i <= size; i++) {
+        for (int j = 0; j <= size; j++) {
             cout << i << " X " << j << " = " << i * j << endl;
         }
+        cout << endl;
+    }
+}
+
+void printTable(const int &size) {
+    cout << setw(5) << right << " ";
+
+    for (int i = 0; i <= size; i++) {
+        cout << setw(5) << right << i;
     }
 
+    cout << endl;
+
+    for (int i = 0; i <= size; i++) {
+        cout << setw(5) << right << i;
+
+        for (int j = 0; j <= size; j++) {
+            cout << setw(5) << right << j * i;
+        }
+        cout << endl;
+    }
+}
+
+
+int main()
+{
+    string choice;
+    int size;
+
+    cout << "Do you want to visualize the result as a 'list' or as a 'table'? ";
+    cin >> choice;
+
+    std::for_each(choice.begin(), choice.end(), [](char& c) {c = ::toupper(c); });
+
+    cout << "Enter the table size: ";
+    cin >> size;
+
+    while (choice != "LIST" && choice != "TABLE") {
+        cout << "Sorry, wrong option. Please try again." << endl;
+        cin >> choice;
+
+        std::for_each(choice.begin(), choice.end(), [](char& c) {c = ::toupper(c); });
+    }
+
+    if (choice == "LIST") {
+        printList(size);
+    }
+    else {
+        printTable(size);
+    }
 }
