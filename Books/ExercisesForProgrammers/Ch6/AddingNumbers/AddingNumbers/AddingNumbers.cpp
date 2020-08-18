7488d2302aaabba4e8077b7edaa627310c73e580
Carlo Messere <messerecarlo27@gmail.com>
2020-08-19 00:02:16 +0200

    AddingNumbers final implementation

--- Books/ExercisesForProgrammers/Ch6/AddingNumbers/AddingNumbers/AddingNumbers.cpp
+++ Books/ExercisesForProgrammers/Ch6/AddingNumbers/AddingNumbers/AddingNumbers.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <numeric>
 
 using namespace std;
 
+void getInt(int &input) {
+	cout << "How many numbers do you want to add? ";
+	cin >> input;
+}
+
+double getDouble(int &count) {
+	double input;
+
+	cout << "Enter a number: ";
+	cin >> input;
+
+	while (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		input = 0;
+	}
+	count++;
+	return input;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	int numbersCount, counter = 0;
+	vector<double> numbers;
+
+	getInt(numbersCount);
+
+	while (counter < numbersCount) {
+		numbers.push_back(getDouble(counter));
+	}
+
+	cout << "The total is: " << std::accumulate(numbers.begin(), numbers.end(), 0);
 }
