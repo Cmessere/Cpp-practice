d06be71fd831d5345165ff8cb7738e5367791671
Carlo Messere <messerecarlo27@gmail.com>
2020-08-19 00:41:00 +0200

    Looping for correct input and selective output

--- Books/ExercisesForProgrammers/Ch6/HandlingBadInput/HandlingBadInput/HandlingBadInput.cpp
+++ Books/ExercisesForProgrammers/Ch6/HandlingBadInput/HandlingBadInput/HandlingBadInput.cpp
@@ -4,5 +4,25 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	double rate;
+
+	while(1)
+	{
+		cout << "What is the rate of return? ";
+		cin >> rate;
+
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+			cout << "Only numbers allowed. Please try again" << endl;
+		}
+		else if (rate == 0)
+			cout << "Hey, you cannot divide by 0, it can destroy the universe." << endl;		
+		else {
+			break;
+		}
+	}
+
+	cout << "It will take " << 72/rate << " years to double your initial investment.";
 }
