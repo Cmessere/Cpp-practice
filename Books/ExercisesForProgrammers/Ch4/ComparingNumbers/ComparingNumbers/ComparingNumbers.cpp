bdf9b64e416ce9917841de03b4a1d82d14237371
Carlo Messere <messerecarlo27@gmail.com>
2020-08-18 03:43:51 +0200

    ComparingNumbers basic implementation

---
+++ Books/ExercisesForProgrammers/Ch4/ComparingNumbers/ComparingNumbers/ComparingNumbers.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+double getDouble(const string question) {
+	double input;
+
+	cout << "Enter the "<< question << " number: ";
+	cin >> input;
+
+	while (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Only numbers allowed. Please try again" << endl;
+		cin >> input;
+	}
+	return input;
+}
+
+int main()
+{
+	double first, second, third, max = std::numeric_limits<double>::min();
+	
+	first = getDouble("first");
+	second = getDouble("second");
+	third = getDouble("third");
+
+	if (first == second || second == third || third == first) {
+		cout << "One of the numbers is equal to the others, exiting..";
+		return 1;
+	}
+
+	if(first > max)
+		max = first;
+	if(second > max)
+		max = second;
+	if(third > max)
+		max = third;
+	
+	cout << "The largest number is: " << max << endl;
+}
