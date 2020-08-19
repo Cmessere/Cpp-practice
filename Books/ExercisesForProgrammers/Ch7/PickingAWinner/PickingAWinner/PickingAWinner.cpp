1384f6219901c34cada157dc4b66df20619b5d37
Carlo Messere <messerecarlo27@gmail.com>
2020-08-19 03:58:40 +0200

    PickingAWinner basic implementation

--- Books/ExercisesForProgrammers/Ch7/PickingAWinner/PickingAWinner/PickingAWinner.cpp
+++ Books/ExercisesForProgrammers/Ch7/PickingAWinner/PickingAWinner/PickingAWinner.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand(time(NULL));
+    string contestant;
+    vector<string> list;
+
+    do {
+        cout << "Enter a name: ";
+        getline(cin, contestant);
+
+        if(!contestant.empty())
+            list.push_back(contestant);
+
+    } while (!contestant.empty());
+
+    cout << "The winner is " << list[rand() % list.size()];
 }
