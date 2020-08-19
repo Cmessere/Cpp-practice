47b1a580d0a7e0b2757dfa2ea97bd47fb9f524d2
Carlo Messere <messerecarlo27@gmail.com>
2020-08-19 18:17:00 +0200

    asking on which field to sort

--- Books/ExercisesForProgrammers/Ch7/SortingRecords/SortingRecords/SortingRecords.cpp
+++ Books/ExercisesForProgrammers/Ch7/SortingRecords/SortingRecords/SortingRecords.cpp
@@ -14,8 +14,14 @@ struct employee {
 };
 
 void initializeEmployees(vector<employee> &list);
-void sortEmployees(vector<employee> &list);
+void sortEmployees(vector<employee> &list, const string field);
 void printEmployees(const vector<employee> &list);
+
+void sortOnSurname(vector<employee>& list);
+void sortOnDate(vector<employee>& list);
+void sortOnPosition(vector<employee>& list);
+
+void askForFieldToSort(std::string& choice);
 
 void initializeEmployees(vector<employee> &list) {
     employee first, second, third, fourth, fifth, sixth;
@@ -53,8 +59,54 @@ void initializeEmployees(vector<employee> &list) {
     list.insert(list.end(), { first, second, third, fourth, fifth, sixth });
 }
 
-void sortEmployees(vector<employee> &list) {
+void sortEmployees(vector<employee> &list, const string field) {
 
+    if (field == "SURNAME") {
+        sortOnSurname(list);
+    }
+    else if (field == "DATE") {
+        sortOnDate(list);
+    }
+    else {
+        sortOnPosition(list);
+    }
+
+}
+
+void printEmployees(const vector<employee> &list) {
+    cout << setw(20) << "Name" << setw(20) << " Position" << setw(20) << " Separation Date" << endl;
+
+    for (auto e : list) {
+        cout << setw(20) << e.name + " " + e.surname << setw(20) << e.position << setw(20) << e.date << endl;
+    }
+}
+
+int main()
+{
+    vector<employee> employeesList;
+    string choice;
+
+    askForFieldToSort(choice);
+    initializeEmployees(employeesList);
+    sortEmployees(employeesList, choice);
+    printEmployees(employeesList);
+}
+
+void askForFieldToSort(std::string& choice)
+{
+    cout << "Do you want to sort on date, surname or position? ";
+    cin >> choice;
+
+    std::for_each(choice.begin(), choice.end(), [](char& c) {c = ::toupper(c); });
+
+
+    while (choice != "DATE" && choice != "SURNAME" && choice != "POSITION") {
+        cout << "Sorry, not a valid option. Please try again" << endl;
+        cin >> choice;
+    }
+}
+
+void sortOnSurname(vector<employee>& list) {
     sort(list.begin(), list.end(), [](const employee& lhs, const employee& rhs)
         {
             if (lhs.surname != rhs.surname)
@@ -67,23 +119,32 @@ void sortEmployees(vector<employee> &list) {
             }
 
         });
-
 }
 
-void printEmployees(const vector<employee> &list) {
-    cout << setw(20) << "Name" << setw(20) << " Position" << setw(20) << " Separation Date" << endl;
-
-    for (auto e : list) {
-        cout << setw(20) << e.name + " " + e.surname << setw(20) << e.position << setw(20) << e.date << endl;
-    }
+void sortOnDate(vector<employee>& list) {
+    sort(list.begin(), list.end(), [](const employee& lhs, const employee& rhs)
+        {
+            if (lhs.date != rhs.date)
+            {
+                return lhs.date < rhs.date;
+            }
+            else
+            {
+                return lhs.date < rhs.date;
+            }
+        });
 }
 
-
-int main()
-{
-    vector<employee> employeesList;
-
-    initializeEmployees(employeesList);
-    sortEmployees(employeesList);
-    printEmployees(employeesList);
+void sortOnPosition(vector<employee>& list) {
+    sort(list.begin(), list.end(), [](const employee& lhs, const employee& rhs)
+        {
+            if (lhs.position != rhs.position)
+            {
+                return lhs.position < rhs.position;
+            }
+            else
+            {
+                return lhs.position < rhs.position;
+            }
+        });
 }
