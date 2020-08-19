66332c24077d46cd05de6f90f9a3000363293b6a
Carlo Messere <messerecarlo27@gmail.com>
2020-08-19 03:31:36 +0200

    Reading employees from file

--- Books/ExercisesForProgrammers/Ch7/EmployeeListRemoval/EmployeeListRemoval/EmployeeListRemoval.cpp
+++ Books/ExercisesForProgrammers/Ch7/EmployeeListRemoval/EmployeeListRemoval/EmployeeListRemoval.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include <list>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-    list<string> employees = { "John Smith", "Jackie Jackson","Chris Jones","Amanda Cullen","Jeremy Goodwin" };
-    string targetToRemove;
+    list<string> employees;
+    string targetToRemove, employeeFromFile;
+
+    ifstream employeeFile;
+
+    employeeFile.open("employees.txt");
+    if (!employeeFile) {
+        cout << "Unable to open file";
+        exit(1);
+    }
+
+    while (getline(employeeFile, employeeFromFile)) {
+        employees.push_back(employeeFromFile);
+    }
 
     while (1) {
         cout << "There are " << employees.size() << " employees" << endl;
