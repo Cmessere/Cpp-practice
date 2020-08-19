80fca9b4c6250d4abb78075074e726ed7bc07569
Carlo Messere <messerecarlo27@gmail.com>
2020-08-19 16:20:58 +0200

    replacing vowels with digits

--- Books/ExercisesForProgrammers/Ch7/PasswordGenerator/PasswordGenerator/PasswordGenerator.cpp
+++ Books/ExercisesForProgrammers/Ch7/PasswordGenerator/PasswordGenerator/PasswordGenerator.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 void getInput(const string& question, int& input);
+void convertVowelsToDigits(string& password, const list<string> vowels);
 string getRandomChar(const list<string> & list);
 string getRandomDigit();
 
@@ -27,10 +28,8 @@ string getRandomChar(const list<string> &list) {
 	random_device engn;
 	uniform_int_distribution<size_t> distribution(0, list.size()-1);
 	
-	int random = distribution(engn);
-
 	auto it = list.begin();
-	advance(it , random);
+	advance(it , distribution(engn));
 	
 	return *it;
 }
@@ -39,16 +38,24 @@ string getRandomDigit() {
 	random_device engn;
 	uniform_int_distribution<size_t> distribution(0, 10);
 	
-	//int random = distribution(engn);
-
 	return std::to_string(distribution(engn));
 }
 
+void convertVowelsToDigits(string& password, const list<char> vowels) {
+	for (int i = 0; i < password.size(); i++) {
+		auto it = std::find(vowels.begin(), vowels.end(), password[i]);
+
+		if (it != vowels.end()) {
+			password.replace(i, 1, getRandomDigit());
+		}
+	}
+}
 
 int main()
 {
 	int length, specialCount, numberCount;
 	list<string> symbols = { "%", "$", "!", "&", "?" };
+	list<char> vowels = { 'a', 'e', 'i', 'o', 'u' };
 	list<string> chars = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
 		"m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
@@ -70,7 +77,8 @@ int main()
 		password += getRandomChar(chars);
 	}
 
+	convertVowelsToDigits(password, vowels);
+
 	random_shuffle(password.begin(), password.end());
 	cout << "Your password is: " << endl << password;
-
 }
