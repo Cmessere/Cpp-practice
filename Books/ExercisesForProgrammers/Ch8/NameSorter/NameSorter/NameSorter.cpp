f32ff1315547952d19f86cac56badb89bfc08a61
Carlo Messere <messerecarlo27@gmail.com>
2020-08-19 22:40:08 +0200

    NameSorter implementation

--- Books/ExercisesForProgrammers/Ch8/NameSorter/NameSorter/NameSorter.cpp
+++ Books/ExercisesForProgrammers/Ch8/NameSorter/NameSorter/NameSorter.cpp
@@ -1,20 +1,49 @@
-// NameSorter.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
-//
-
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void sortNames(vector<string> &names);
+void getNamesFromFile(vector<string>& names);
+void printNames(const vector<string> &names);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	vector<string> names;
+
+	getNamesFromFile(names);
+	sortNames(names);
+	printNames(names);
 }
 
-// Per eseguire il programma: CTRL+F5 oppure Debug > Avvia senza eseguire debug
-// Per eseguire il debug del programma: F5 oppure Debug > Avvia debug
+void getNamesFromFile(vector<string>& names)
+{
+	string input;
+	ifstream nameFile;
 
-// Suggerimenti per iniziare: 
-//   1. Usare la finestra Esplora soluzioni per aggiungere/gestire i file
-//   2. Usare la finestra Team Explorer per connettersi al controllo del codice sorgente
-//   3. Usare la finestra di output per visualizzare l'output di compilazione e altri messaggi
-//   4. Usare la finestra Elenco errori per visualizzare gli errori
-//   5. Passare a Progetto > Aggiungi nuovo elemento per creare nuovi file di codice oppure a Progetto > Aggiungi elemento esistente per aggiungere file di codice esistenti al progetto
-//   6. Per aprire di nuovo questo progetto in futuro, passare a File > Apri > Progetto e selezionare il file con estensione sln
+	nameFile.open("names.txt");
+
+	if (!nameFile.is_open()) {
+		cout << "Could not open file";
+		exit(1);
+	}
+
+	while (getline(nameFile, input)) {
+		names.push_back(input);
+	}
+}
+
+void sortNames(vector<string> &names) {
+	sort(names.begin(), names.end());
+}
+
+void printNames(const vector<string> &names) {
+	cout << "Total of " << names.size() << " names" << endl;
+
+	for (auto n : names) {
+		cout << n << endl;
+	}
+}
