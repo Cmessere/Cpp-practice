e9fd8df0380059d2cbdc3d45a0b91a8eb5c177fd
Carlo Messere <messerecarlo27@gmail.com>
2020-08-19 03:25:12 +0200

    EmployeeListRemoval basic implementation

--- Books/ExercisesForProgrammers/Ch7/EmployeeListRemoval/EmployeeListRemoval/EmployeeListRemoval.cpp
+++ Books/ExercisesForProgrammers/Ch7/EmployeeListRemoval/EmployeeListRemoval/EmployeeListRemoval.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <list>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    list<string> employees = { "John Smith", "Jackie Jackson","Chris Jones","Amanda Cullen","Jeremy Goodwin" };
+    string targetToRemove;
+
+    while (1) {
+        cout << "There are " << employees.size() << " employees" << endl;
+        for (auto i : employees) {
+            cout << i << endl;
+        }
+        cout << endl;
+
+        cout << "Enter an employee name to remove, or 'stop' : ";
+        getline(cin,targetToRemove);
+
+        if (targetToRemove == "stop")
+            break;
+
+        auto it = find(employees.begin(), employees.end(), targetToRemove);
+
+        if (it == employees.end()) {
+            cout << "The inserted employee does not exist!";
+        }
+        else {
+            employees.remove(targetToRemove);
+        }
+    }
 }
