99e05a3a1ac000365da4c0ef65455315fda74e41
Carlo Messere <messerecarlo27@gmail.com>
2020-08-19 16:56:16 +0200

    copying password to clipboard

--- Books/ExercisesForProgrammers/Ch7/PasswordGenerator/PasswordGenerator/PasswordGenerator.cpp
+++ Books/ExercisesForProgrammers/Ch7/PasswordGenerator/PasswordGenerator/PasswordGenerator.cpp
@@ -3,54 +3,23 @@
 #include <string>
 #include <random>
 #include <time.h>
+#include <Windows.h> 
+#include <winuser.h>
+#include <conio.h>
+#include <stdio.h>
+#include <cstdlib>
 
 using namespace std;
 
 void getInput(const string& question, int& input);
 void convertVowelsToDigits(string& password, const list<string> vowels);
 void generatePassword();
+void copyPasswordToClipboard(int& selectedOption, int iterations, std::vector<std::string>& passwords);
 string getRandomChar(const list<string> & list);
 string getRandomDigit();
+void toClipboard(const string& s);
 
-void getInput(const string& question, int& input) {
-	cout << question;
-	cin >> input;
 
-	while (cin.fail())
-	{
-		cin.clear();
-		cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		cout << "Only numbers allowed. Please try again" << endl;
-		cin >> input;
-	}
-}
-
-string getRandomChar(const list<string> &list) {
-	random_device engn;
-	uniform_int_distribution<size_t> distribution(0, list.size()-1);
-	
-	auto it = list.begin();
-	advance(it , distribution(engn));
-	
-	return *it;
-}
-
-string getRandomDigit() {
-	random_device engn;
-	uniform_int_distribution<size_t> distribution(0, 10);
-	
-	return std::to_string(distribution(engn));
-}
-
-void convertVowelsToDigits(string& password, const list<char> vowels) {
-	for (int i = 0; i < password.size(); i++) {
-		auto it = std::find(vowels.begin(), vowels.end(), password[i]);
-
-		if (it != vowels.end()) {
-			password.replace(i, 1, getRandomDigit());
-		}
-	}
-}
 
 int main()
 {
@@ -59,7 +28,7 @@ int main()
 
 void generatePassword()
 {
-	int length, specialCount, numberCount, iterations;
+	int length, specialCount, numberCount, iterations, selectedOption;
 	list<string> symbols = { "%", "$", "!", "&", "?" };
 	list<char> vowels = { 'a', 'e', 'i', 'o', 'u' };
 	list<string> chars = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
@@ -96,4 +65,76 @@ void generatePassword()
 		cout << "Your password number " << k+1 << " is: " << endl << password << endl << endl;
 	}	
 
+	copyPasswordToClipboard(selectedOption, iterations, passwords);
+}
+
+void copyPasswordToClipboard(int& selectedOption, int iterations, std::vector<std::string>& passwords)
+{
+	cout << "Which password do you want copied? ";
+	cin >> selectedOption;
+
+	while (cin.fail() || (selectedOption <= 0 || selectedOption > iterations)) {
+		cin.clear();
+		cin.ignore(100, '\n');
+		cout << "Sorry, that is not a valid option. Please try again: ";
+		cin >> selectedOption;
+	}
+
+	cout << "copying...";
+	toClipboard(passwords[selectedOption - 1]);
+}
+
+void getInput(const string& question, int& input) {
+	cout << question;
+	cin >> input;
+
+	while (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(100, '\n');
+		cout << "Only numbers allowed. Please try again" << endl;
+		cin >> input;
+	}
+}
+
+string getRandomChar(const list<string>& list) {
+	random_device engn;
+	uniform_int_distribution<size_t> distribution(0, list.size() - 1);
+
+	auto it = list.begin();
+	advance(it, distribution(engn));
+
+	return *it;
+}
+
+string getRandomDigit() {
+	random_device engn;
+	uniform_int_distribution<size_t> distribution(0, 10);
+
+	return std::to_string(distribution(engn));
+}
+
+void convertVowelsToDigits(string& password, const list<char> vowels) {
+	for (int i = 0; i < password.size(); i++) {
+		auto it = std::find(vowels.begin(), vowels.end(), password[i]);
+
+		if (it != vowels.end()) {
+			password.replace(i, 1, getRandomDigit());
+		}
+	}
+}
+
+void toClipboard(const string& s) {
+	OpenClipboard(0);
+	EmptyClipboard();
+	HGLOBAL hg = GlobalAlloc(GMEM_MOVEABLE, s.size());
+	if (!hg) {
+		CloseClipboard();
+		return;
+	}
+	memcpy(GlobalLock(hg), s.c_str(), s.size());
+	GlobalUnlock(hg);
+	SetClipboardData(CF_TEXT, hg);
+	CloseClipboard();
+	GlobalFree(hg);
 }
