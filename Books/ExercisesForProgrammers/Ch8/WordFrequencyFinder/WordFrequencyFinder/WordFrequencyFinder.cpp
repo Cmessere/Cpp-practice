d33b09ce1d179404d175bb5647ba6bafcec72ec5
Carlo Messere <messerecarlo27@gmail.com>
2020-08-20 04:11:31 +0200

    implemented WorldFrequencyFinder

--- Books/ExercisesForProgrammers/Ch8/WordFrequencyFinder/WordFrequencyFinder/WordFrequencyFinder.cpp
+++ Books/ExercisesForProgrammers/Ch8/WordFrequencyFinder/WordFrequencyFinder/WordFrequencyFinder.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <algorithm>
+
 
 using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	map<string, int> occurrences;
+	typedef pair<string, int> pair;
+	vector<pair> vector;
+
+	ifstream inputFile;
+	string word;
+
+	inputFile.open("input.txt");
+
+	if (!inputFile.is_open()) {
+		cout << "Could not open file!";
+		exit(1);
+	}
+
+	while (inputFile >> word) {
+		std::for_each(word.begin(), word.end(), [](char& c) {c = ::tolower(c); });
+
+		map<string, int>::iterator it = occurrences.find(word);
+		if (it != occurrences.end())
+		{
+			occurrences[word]++;
+		}
+		else {
+			occurrences[word] = 1;
+		}
+
+	}
+
+	copy(occurrences.begin(), occurrences.end(), back_inserter<std::vector<pair>>(vector));
+
+	std::sort(vector.begin(), vector.end(),[](const pair& l, const pair& r) {
+				return l.second > r.second;
+		});
+
+	for (auto i : vector) {
+		cout << i.first << ": " << i.second <<endl;
+	}
 }
