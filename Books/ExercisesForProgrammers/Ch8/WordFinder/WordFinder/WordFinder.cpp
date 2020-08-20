671d43710e03ed296443378450ec77d4e680de1f
Carlo Messere <messerecarlo27@gmail.com>
2020-08-20 03:25:13 +0200

    WordFinder basic implementation

--- Books/ExercisesForProgrammers/Ch8/WordFinder/WordFinder/WordFinder.cpp
+++ Books/ExercisesForProgrammers/Ch8/WordFinder/WordFinder/WordFinder.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+bool replace(string& str, const string& from, const string& to);
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    ifstream inputFile;
+    ofstream outputFile;
+    string input, outputFileName;
+
+    inputFile.open("input.txt");
+
+    cout << "What is the name of the output file? ";
+    cin >> outputFileName;
+
+    outputFileName += ".txt";
+    outputFile.open(outputFileName);
+
+    while (getline(inputFile, input)) {
+        replace(input, "utilize", "use");
+
+        outputFile << input << endl;
+    }
+
+}
+
+bool replace(string& str, const string& toReplace, const string& newSubstr) {
+
+    size_t position = str.find(toReplace);
+
+    if (position == string::npos)
+        return false;
+
+    str.replace(position, toReplace.length(), newSubstr);
+    return true;
 }
