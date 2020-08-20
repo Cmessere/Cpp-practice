66ac1fcef7db9761bdf7e33dd6d1b650cd002604
Carlo Messere <messerecarlo27@gmail.com>
2020-08-21 01:24:43 +0200

    completed trivia loop

--- Books/ExercisesForProgrammers/Ch10/TriviaApp/TriviaApp/TriviaApp.cpp
+++ Books/ExercisesForProgrammers/Ch10/TriviaApp/TriviaApp/TriviaApp.cpp
@@ -1,22 +1,62 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
 #include <curl/curl.h>
 #include <json/json.h> 
 
 using namespace std;
 
 void fetchData(CURL* curl, std::string& readBuffer, CURLcode& res);
-void parseJson(const CURLcode& res, const string& readBuffer);
+void parseJson(const CURLcode& res, const string& readBuffer, vector < tuple<string, vector<string>>>& trivia);
 static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
+void askQuestion(vector < tuple<string, vector<string>>> &trivia);
 
 int main()
 {
+    vector < tuple<string, vector<string>>> trivia;
     CURLcode res;
     string readBuffer;
     CURL* curl;
     curl = curl_easy_init();
 
     fetchData(curl, readBuffer, res);
-    parseJson(res, readBuffer);
+    parseJson(res, readBuffer, trivia);
+    askQuestion(trivia);
+}
+
+void askQuestion(vector < tuple<string, vector<string>>>& trivia) {
+    int score = 0, correctIndex, userAnswer;
+    string correctAnswer;
+    vector<string> possibleAnswers;
+
+    for (auto q : trivia) {
+        possibleAnswers = get<1>(q);
+        correctAnswer = possibleAnswers[0];
+
+        std::random_shuffle(possibleAnswers.begin(), possibleAnswers.end());
+
+        auto it = (find(possibleAnswers.begin(), possibleAnswers.end(), correctAnswer));
+        correctIndex = std::distance(possibleAnswers.begin(), it);
+
+        cout << get<0>(q) << endl;
+        for (int i = 0; i < possibleAnswers.size(); i++) {
+            cout << i + 1 << ") " << possibleAnswers[i] << endl;
+        }
+
+        cin >> userAnswer;
+
+        if (correctIndex == userAnswer-1) {
+            cout << "That is correct! Good job!" << endl;
+            score++;
+        }
+        else {
+            cout << "That is not correct unfortunately." << endl;
+            cout << "The correct answer was number " << correctIndex+1 << endl;
+        }
+
+        cout << "Next question!\n\n";
+    }
+
 }
 
 void fetchData(CURL* curl, std::string& readBuffer, CURLcode& res)
@@ -32,7 +72,7 @@ void fetchData(CURL* curl, std::string& readBuffer, CURLcode& res)
     }
 }
 
-void parseJson(const CURLcode& res, const string& readBuffer)
+void parseJson(const CURLcode& res, const string& readBuffer, vector < tuple<string, vector<string>>>&trivia)
 {
     if (res == CURLE_OK) {
         Json::Value data;
@@ -46,8 +86,23 @@ void parseJson(const CURLcode& res, const string& readBuffer)
             std::cout << "error" << std::endl;
         }
 
-        const Json::Value& questionsJson = data;
-        cout << data["results"];
+        const Json::Value& questionsJson = data["results"];
+
+        for (auto question : questionsJson) {
+            vector<string> answers;
+            tuple<string, vector<string>> triviaTuple;
+
+            answers.push_back(question["correct_answer"].asString());
+
+            for (auto a : question["incorrect_answers"]) {
+                answers.push_back(a.asString());
+            }
+
+            get<0>(triviaTuple) = question["question"].asString();
+            get<1>(triviaTuple) = answers;
+
+            trivia.push_back(triviaTuple);
+        }
     }
     else {
         cerr << "Could not fetch questions, check internet connection and try again\n";
